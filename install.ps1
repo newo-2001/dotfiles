e1af1cb3d8bf94731562d9150ce4b84b0fa10111
newo-2001 <o.elderbroek@gmail.com>
2023-08-03 16:54:30 +0200

    Make install scripts install external depencencies

--- install.ps1
+++ install.ps1
@@ -8,15 +8,18 @@ $WSL_ENV = "USERPROFILE/up";
     $null = New-Item -Path $mirror -Name "Microsoft.PowerShell_profile.ps1" -ItemType SymbolicLink -Value $source -Force
 }
 
-# Test if Oh-My-Posh is installed
-if (Test-Path "env:POSH_THEMES_PATH")
+if (!(Get-Command "oh-my-posh" -ErrorAction SilentlyContinue))
 {
-    # Install Oh-My-Posh Theme
-    Write-Output "Installing Oh-My-Posh theme..."
-    $source = Join-Path "Oh-My-Posh" "custom.omp.json"
-    $null = New-Item -Path $Env:POSH_THEMES_PATH -Name "custom.omp.json" -ItemType SymbolicLink -Value $source -Force
-    $WSL_ENV += ":POSH_THEMES_PATH/up";
+    # Install Oh-My-Posh
+    Write-Output "Installing Oh-My-Posh..."
+    winget install JanDeDobbeleer.OhMyPosh -s winget
 }
+
+# Install Oh-My-Posh Theme
+Write-Output "Installing Oh-My-Posh theme..."
+$source = Join-Path "Oh-My-Posh" "custom.omp.json"
+$null = New-Item -Path $Env:POSH_THEMES_PATH -Name "custom.omp.json" -ItemType SymbolicLink -Value $source -Force
+$WSL_ENV += ":POSH_THEMES_PATH/up";
 
 # Configure shared environment variables for WSL
 Write-Output "Sharing environment variables with WSL..."
@@ -39,9 +42,16 @@ Write-Output "Sharing environment variables with WSL..."
 if (Get-Command "git" -ErrorAction SilentlyContinue)
 {
     # Install .gitconfig
-    Write-Output "Installing Git Configuration"
+    Write-Output "Installing Git configuration..."
     $source = Join-Path "Git" ".gitconfig"
     $null = New-Item -Path $HOME -Name ".gitconfig" -ItemType SymbolicLink -Value $source -Force
+}
+
+if (!(Get-Command "delta" -ErrorAction SilentlyContinue))
+{
+    # Install Delta
+    Write-Output "Installing Delta..."
+    winget install dandavison.delta
 }
 
 Write-Output "Done."
--- install.sh
+++ install.sh
@@ -5,4 +5,25 @@ echo "Installing Bash Profile..."
 ln -sf $(realpath "Bash/.bashrc") ~/.bashrc
 ln -sf $(realpath "Bash/.profile") ~/.profile 
 
+echo "Installing Git Profile..."
+ln -sf $(realpath "Git/.gitconfig") ~/.gitconfig
+
+if ! hash brew
+then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! hash delta
+then
+    echo "Installing Delta..."
+    brew reinstall git-delta
+fi
+
+if ! hash oh-my-posh
+then
+    echo "Installing Oh-My-Posh..."
+    brew install jandedobbeleer/oh-my-posh/oh-my-posh
+fi
+
 echo "Done."
