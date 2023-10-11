7320fc43c8014477a66d1752f19b0c13ce230c17
newo-2001 <o.elderbroek@gmail.com>
2023-10-11 10:42:46 +0200

    Ignore blank lines and comments in VSCode extension install script

--- VSCode/install-extensions.ps1
+++ VSCode/install-extensions.ps1
@@ -1,7 +1,15 @@
 Write-Output "Installing VSCode extensions..."
 $arguments = ""
+
 foreach ($extension in Get-Content (Join-Path "VSCode" "extensions.txt"))
 {
+    if (($extension -eq "") -or $extension -match "^#.*")
+    {
+        continue
+    }
+
+    $extension | Write-Output
     $arguments += "--install-extension $extension --force "
 }
+
 Start-Process "code" -ArgumentList $arguments -NoNewWindow -Wait
