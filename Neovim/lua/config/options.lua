e08c3b43943f5b37e09900a8f40ba0baba09e448
newo-2001 <o.elderbroek@gmail.com>
2024-01-10 14:55:42 +0100

    Configured neovim to do indentation correctly

--- Neovim/lua/config/options.lua
+++ Neovim/lua/config/options.lua
@@ -1,3 +1,7 @@
 vim.opt.mouse = "a" -- Enable mouse clicks everywhere
 vim.opt.wildmode = "list,longest" -- Enable command tab completion
 vim.opt.number = true -- Enable line numbers
+vim.opt.tabstop = 4 -- Tab size 4 spaces
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true -- Indent newlines with tabs
