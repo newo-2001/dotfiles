158f9699d7b885b8fffd2d8ac318f71b2467a9b9
newo-2001 <o.elderbroek@gmail.com>
2024-01-10 15:27:45 +0100

    Enable neovim 'Hard' mode

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -3,6 +3,7 @@ require("bootstrap")
 
 -- Load options
 require("config.options")
+require("config.mappings")
 
 -- Configure lazy plugins
 require("lazy").setup({})
---
+++ Neovim/lua/config/mappings.lua
@@ -0,0 +1,6 @@
+-- Enable 'hard' mode
+local function noop() end
+vim.keymap.set('n', "<Up>", noop)
+vim.keymap.set('n', "<Down>", noop)
+vim.keymap.set('n', "<Left>", noop)
+vim.keymap.set('n', "<Right>", noop)
