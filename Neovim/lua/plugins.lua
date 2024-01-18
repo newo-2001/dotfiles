bb409ce76a3ecb768fb96a18be8bc6d8404d146c
newo-2001 <o.elderbroek@gmail.com>
2024-01-18 11:43:00 +0100

    Add terminal to Neovim

--- Neovim/lua/plugins.lua
+++ Neovim/lua/plugins.lua
@@ -33,5 +33,17 @@ return {
     {
         "rebelot/kanagawa.nvim",
         config = function() require("kanagawa").load("wave") end
+    },
+    {
+        "voldikss/vim-floaterm",
+        config = function()
+            vim.keymap.set('n', "<C-L>", ":FloatermToggle<CR>")
+            vim.keymap.set('t', "<C-L>", "<C-\\><C-n>:FloatermToggle<CR>")
+            vim.api.nvim_set_hl(0, "Floaterm", { bg = "#212529" })
+            vim.api.nvim_set_hl(0, "FloatermBorder", { bg = "#212529" })
+            vim.g.floaterm_shell = "bash --login"
+            vim.g.floaterm_width = 0.8
+            vim.g.floaterm_height = 0.8
+        end
     }
 }
