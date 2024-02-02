21d4a824b05cc5eaaffeaa7d9992ed3ec16555da
newo-2001 <o.elderbroek@gmail.com>
2024-02-02 13:03:39 +0100

    Configure Neovim diagnostic messages

--- Neovim/lua/config/options.lua
+++ Neovim/lua/config/options.lua
@@ -9,5 +9,19 @@ vim.opt.ignorecase = true -- Search case insensitive
 vim.opt.smartcase = true -- Except when using capitals
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard as default
 vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.path = ".,**"
+vim.opt.path = ".,**" -- Allow searching nested directories for files
 vim.opt.pumheight = 10 -- Max number of items to show in popup window
+
+vim.diagnostic.config({
+    underline = false, -- Disable underlining for diagnostic messages
+    virtual_text = {
+        prefix = ''
+    }
+})
+
+-- Configure diagnostic icons in the sidebar
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
