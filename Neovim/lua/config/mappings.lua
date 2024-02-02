cb39637deaafb162de2bc450b4f70e62d5b18d39
newo-2001 <o.elderbroek@gmail.com>
2024-02-02 11:10:24 +0100

    Configure Neovim Code actions

--- Neovim/lua/config/mappings.lua
+++ Neovim/lua/config/mappings.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', "<Up>", noop)
 vim.keymap.set('n', "<Down>", noop)
 vim.keymap.set('n', "<Left>", noop)
 vim.keymap.set('n', "<Right>", noop)
+
+-- Open code actions
+vim.keymap.set('n', "<Leader>ca", vim.lsp.buf.code_action)
