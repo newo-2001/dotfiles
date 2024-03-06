032da52a31c202226c64187d2db54e31da925a87
newo-2001 <o.elderbroek@gmail.com>
2024-03-06 16:50:49 +0100

    Neovim hybrid line numbers

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -4,3 +4,6 @@ require("config.options")
 require("config.mappings")
 
 require("winbar")
+
+vim.api.nvim_create_autocmd("InsertEnter", { callback = function() vim.opt.relativenumber = false end })
+vim.api.nvim_create_autocmd("InsertLeave", { callback = function() vim.opt.relativenumber = true end })
