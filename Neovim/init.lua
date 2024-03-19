56c1b33e38661e6a86f57737e36f6c361fbec02b
newo-2001 <o.elderbroek@gmail.com>
2024-03-19 11:49:02 +0100

    Fix git upstream icon in oh-my-posh config

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -9,5 +9,5 @@ require("winbar")
 vim.api.nvim_create_autocmd("InsertEnter", { callback = function() vim.opt.relativenumber = false end })
 vim.api.nvim_create_autocmd("InsertLeave", { callback = function() vim.opt.relativenumber = true end })
 
--- Write on Shift-w as well
+-- Write on :W as well
 vim.api.nvim_create_user_command("W", function() vim.api.nvim_command("write") end, {})
