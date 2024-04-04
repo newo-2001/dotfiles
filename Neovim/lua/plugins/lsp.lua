eabb7b53b11be088f218f558599168c0454f21cb
newo-2001 <o.elderbroek@gmail.com>
2024-04-04 13:32:25 +0200

    Refactor Neovim lsp configuration

--- Neovim/lua/plugins/lsp.lua
+++ Neovim/lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+local utils = require("utils")
 local has_ghcup = vim.fn.executable("ghcup") ~= 0
 
 local function on_attach(client)
@@ -24,15 +25,13 @@ return {
                 "jsonls",       -- Json
                 "yamlls",       -- Yaml
                 "terraformls",  -- Terraform,
-                "clangd",       -- C++
+                "clangd"        -- C++
             }
 
             -- Only install haskell ls if the compiler is also installed
             if has_ghcup then table.insert(language_servers, "hls") end
 
-            require("mason-lspconfig").setup({
-                ensure_installed = language_servers
-            })
+            require("mason-lspconfig").setup({ ensure_installed = language_servers })
         end,
         dependencies = { "williamboman/mason.nvim" }
     },
@@ -41,59 +40,68 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local schemastore = require("schemastore")
+
             local language_servers = {
                 "lua_ls",
                 "texlab",
                 "tsserver",
                 "terraformls",
-                "clangd"
+                "clangd",
+                {
+                    "omnisharp",
+                    opts = {
+                        enable_import_completion = true,
+                        enable_roslyn_analyzers = true,
+                        enable_editor_config_support = true,
+                        analyze_open_document_only = false,
+                        cmd = { "dotnet", vim.fn.stdpath("data") .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" }
+                    }
+                },
+                {
+                    "jsonls",
+                    opts = {
+                        settings = {
+                            json = {
+                                validate = { enable = true },
+                                schemas = schemastore.json.schemas({
+                                    extra = {}
+                                })
+                            }
+                        }
+                    }
+                },
+                {
+                    "yamlls",
+                    opts = {
+                        settings = {
+                            yaml = {
+                                schemaStore = {
+                                    enable = false,
+                                    url = "",
+                                },
+                                schemas = schemastore.yaml.schemas()
+                            }
+                        }
+                    }
+                }
             }
 
             if has_ghcup then table.insert(language_servers, "hls") end
 
             for _, language_server in pairs(language_servers) do
-                lspconfig[language_server].setup({
+                local options = {
                     capabilities = capabilities,
                     on_attach = on_attach
-                })
+                }
+
+                if type(language_server) == "table" then
+                    options = utils.merge_tables(language_server.opts, options)
+                    language_server = language_server[1]
+                end
+
+                lspconfig[language_server].setup(options)
             end
-
-            lspconfig.omnisharp.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                enable_import_completion = true,
-                enable_roslyn_analyzers = true,
-                analyze_open_document_only = false,
-                cmd = { "dotnet", vim.fn.stdpath("data") .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" }
-            })
-
-            local schemastore = require("schemastore")
-            lspconfig.jsonls.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    json = {
-                        validate = { enable = true },
-                        schemas = schemastore.json.schemas({
-                            extra = {}
-                        })
-                    }
-                }
-            })
-
-            lspconfig.yamlls.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    yaml = {
-                        schemaStore = {
-                            enable = false,
-                            url = "",
-                        },
-                        schemas = schemastore.yaml.schemas()
-                    }
-                }
-            })
         end,
         dependencies = {
             "williamboman/mason-lspconfig.nvim",
--- Neovim/lua/utils.lua
+++ Neovim/lua/utils.lua
@@ -8,4 +8,31 @@ utils.isWindows = function()
     return vim.fn.has("win64") or vim.fn.has("win32") or vim.fn.has("win16")
 end
 
+utils.deep_copy = function(obj)
+    if type(obj) ~= "table" then return obj end
+
+    local copy = {}
+    for k, v in pairs(obj) do
+        if type(v) == "table" then
+            v = utils.deep_copy(v)
+        end
+
+        copy[k] = v
+    end
+
+    return copy
+end
+
+utils.merge_tables = function(...)
+    local args = { ... }
+    local merged = {}
+    for _, table in pairs(args) do
+        for k, v in pairs(table) do
+            if merged[k] == nil then merged[k] = v end
+        end
+    end
+
+    return merged
+end
+
 return utils
