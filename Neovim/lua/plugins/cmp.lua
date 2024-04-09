6158f9bc00984756e7c0e513f26cba2c23ef4a71
newo-2001 <o.elderbroek@gmail.com>
2024-04-09 09:19:03 +0200

    Change autocomplete navigation keys to tab & ctrl+tab

--- Neovim/lua/plugins/cmp.lua
+++ Neovim/lua/plugins/cmp.lua
@@ -15,8 +15,8 @@ return {
                     end
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ["<C-k>"] = cmp.mapping.select_prev_item(),
-                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    ["<C-Tab>"] = cmp.mapping.select_prev_item(),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<CR>"] = cmp.mapping.confirm({ select = false })
                 }),
