645a10136208efdc8a5b62a7a0f8b0b6956feedb
newo-2001 <o.elderbroek@gmail.com>
2024-04-24 16:28:21 +0200

    Disable smart line numbers in Neovim Telescope prompt

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -5,15 +5,20 @@ require("config.mappings")
 
 require("winbar")
 
+local line_number_toggle_blacklist = {
+    "alpha",
+    "TelescopePrompt"
+}
+
 -- Toggle relative line numbers when entering and leaving insert mode
 vim.api.nvim_create_autocmd("InsertEnter", { callback = function()
-    if vim.bo.filetype ~= "alpha" then
+    if not vim.tbl_contains(line_number_toggle_blacklist, vim.bo.filetype) then
         vim.opt.relativenumber = false
     end
 end })
 
 vim.api.nvim_create_autocmd("InsertLeave", { callback = function()
-    if vim.bo.filetype ~= "alpha" then
+    if not vim.tbl_contains(line_number_toggle_blacklist, vim.bo.filetype) then
         vim.opt.relativenumber = true
     end
 end })
